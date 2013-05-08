6a624c7880c3b0aec04f02036dc8f7b5345fae7a
Ladislav Vondracek <lawondyss@gmail.com>
2013-05-08 19:25:38 +0200

    Added module waitress.
    Creating a model, presenter and a template for the order.
    Orders are displayed according to the tables. Can they adjust status.
    Existing statuses:
     wait - incoming
     in progress - processing
     done - done
     cancel - canceled by the client
     canceled - waitress confirmed

--- app/WaitressModule/presenters/OrdersPresenter.php
+++ app/WaitressModule/presenters/OrdersPresenter.php
@@ -5,10 +5,36 @@
 
 namespace WaitressModule;
 
+use Nette\DateTime;
+
 class OrdersPresenter extends BasePresenter
 {
+	/** @var \OrdersRepository */
+	private $ordersRepository;
+
+
 	protected function startup()
 	{
 		parent::startup();
+
+		$this->ordersRepository = $this->context->ordersRepository;
+	}
+
+
+	public function renderDefault()
+	{
+		$this->template->tables = $this->ordersRepository->findForWaitress();
+	}
+
+
+	/**
+	 * @param int $id
+	 * @param string $status
+	 */
+	public function handleChangeStatus($id, $status)
+	{
+		$data = array('status' => $status);
+		$this->ordersRepository->update($data, $id);
+		$this->redirect('this');
 	}
 }
---
+++ app/model/OrdersRepository.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Class OrdersRepository
+ */
+
+class OrdersRepository extends BaseRepository
+{
+	public function __construct(DibiConnection $db)
+	{
+		parent::__construct($db);
+
+		$this->table = 'orders';
+	}
+
+
+	/***/
+	public function findForWaitress()
+	{
+		$query =
+			'SELECT [o].*,[t.number],[c.name] AS [client],[i.type],[i.name] AS [item],[i.quantity] ' .
+			'FROM %n AS [o] ' .
+			'LEFT JOIN %n AS [t] ON [o.id_tables]=[t.id] ' .
+			'LEFT JOIN %n AS [c] ON [o.id_clients]=[c.id] ' .
+			'LEFT JOIN %n AS [i] ON [o.id_items]=[i.id] ' .
+			'WHERE [status] NOT IN %l ' .
+			'ORDER BY [ordered]'
+		;
+		return $this->db->query($query, $this->table, 'tables', 'clients', 'items', array('done', 'canceled'))->fetchAssoc('id_tables,type,id');
+	}
+}
